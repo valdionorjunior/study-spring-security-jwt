a4918a0ea94d07ee2ece0a3f17bec388feb8fc03
valdionorjunior <valdionorjunior@outlook.com>
2023-07-03 10:07:22 -0300

    First commit

---
+++ src/main/java/com/juniorrodrigues/study/spring/security/jwt/StudySpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.juniorrodrigues.study.spring.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juniorrodrigues/study/spring/security/jwt/StudySpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorrodrigues.study.spring.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
